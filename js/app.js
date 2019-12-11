18f752554f70d5b3442b1aed24a9326629afb78c
davidsah1991 <45639152+davidsah1991@users.noreply.github.com>
2019-12-11 16:23:26 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ document.addEventListener('deviceready',onDeviceReady, false);
 
 var MyCampusApp = {
     config : {
-        tenant : "TENANT_ID",
+        tenant : "SBCC",
         serverUrl : "https://kryptos.kryptosmobile.com",
         tenantFolder : function(device, tenant) {
             if(device.platform == 'Android') {
